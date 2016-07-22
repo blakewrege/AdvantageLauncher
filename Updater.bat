67fdec6b8a478df03d20dedf6f1346d561c59f92
Blake J. Wrege <bwrege@thermaltech.com>
2016-07-22 09:52:48 -0400

    created development branch

--- Updater.bat
+++
@@ -1,7 +0,0 @@
-Taskkill /IM AdvantageLauncher.exe /F
-IF EXIST Releases Powershell.exe  -ExecutionPolicy Bypass "Remove-Item .\Releases -Force -Recurse"
-mkdir Releases
-Powershell.exe  -ExecutionPolicy Bypass "(New-Object System.Net.WebClient).DownloadFile('https://github.com/gigglesbw4/AdvantageLauncher/archive/master.zip"', 'Releases\currentversion.zip'); "
-Powershell.exe  -ExecutionPolicy Bypass "Add-Type -AssemblyName System.IO.Compression.FileSystem; [System.IO.Compression.ZipFile]::ExtractToDirectory('Releases\currentversion.zip', 'Releases\currentversion'); "
-start Powershell.exe -ExecutionPolicy Bypass "Copy-Item  -Path Releases\currentversion\AdvantageLauncher-master\* -Destination . -Recurse -force; start cmd.exe ' /c echo Updating is complete & pause '"
-
