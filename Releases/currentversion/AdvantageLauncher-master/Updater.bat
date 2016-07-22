41ad5b5946e95267d5ac74c3987d3ea98d1d26c0
Blake J. Wrege <bwrege@thermaltech.com>
2016-07-22 09:45:56 -0400

    fixed updater

--- Releases/currentversion/AdvantageLauncher-master/Updater.bat
+++ Releases/currentversion/AdvantageLauncher-master/Updater.bat
@@ -3,5 +3,4 @@ IF EXIST Releases Powershell.exe  -ExecutionPolicy Bypass "Remove-Item .\Release
 mkdir Releases
 Powershell.exe  -ExecutionPolicy Bypass "(New-Object System.Net.WebClient).DownloadFile('https://github.com/gigglesbw4/AdvantageLauncher/archive/master.zip"', 'Releases\currentversion.zip'); "
 Powershell.exe  -ExecutionPolicy Bypass "Add-Type -AssemblyName System.IO.Compression.FileSystem; [System.IO.Compression.ZipFile]::ExtractToDirectory('Releases\currentversion.zip', 'Releases\currentversion'); "
-start Powershell.exe -ExecutionPolicy Bypass "Copy-Item  -Path Releases\currentversion\AdvantageLauncher-master\* -Destination . -Recurse -force; start cmd.exe ' /c echo Updating is complete & pause '"
-
+start Powershell.exe -ExecutionPolicy Bypass "Copy-Item  -Path Releases\currentversion\AdvantageLauncher -Destination . -Recurse -force; start cmd.exe ' /c echo Updating is complete & pause '"
