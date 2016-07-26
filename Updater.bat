2cc919c6d94c77bdd6e045a900b0d8347fa30a40
Blake J. Wrege <bwrege@thermaltech.com>
2016-07-26 17:34:27 -0400

    fixed update ping

--- Updater.bat
+++ Updater.bat
@@ -4,7 +4,6 @@ IF EXIST Releases Powershell.exe  -ExecutionPolicy Bypass "Remove-Item .\Release
 mkdir Releases
 Powershell.exe  -ExecutionPolicy Bypass "(New-Object System.Net.WebClient).DownloadFile('https://github.com/gigglesbw4/AdvantageLauncher/archive/master.zip"', 'Releases\currentversion.zip'); "
 Powershell.exe  -ExecutionPolicy Bypass "Add-Type -AssemblyName System.IO.Compression.FileSystem; [System.IO.Compression.ZipFile]::ExtractToDirectory('Releases\currentversion.zip', 'Releases\currentversion'); "
-Taskkill /IM AdvantageLauncher.exe /F
 if "%arg1%"=="yes" (
 GOTO YES
  )
@@ -12,6 +11,7 @@ if %arg1 NEQ "yes" (
 GOTO NO
 )
 :YES
+Taskkill /IM AdvantageLauncher.exe /F
 start /MIN Powershell.exe -ExecutionPolicy Bypass -windowstyle hidden "Copy-Item  -Path Releases\currentversion\AdvantageLauncher-master\* -Destination . -Recurse -force; start cmd.exe ' /c echo Updating is complete & pause '"
 exit 0
 :NO
